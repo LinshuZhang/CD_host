c4c16c4bab372c84fcc93fe4976e0d440d40ad26
LinshuZhang <z247124910@gmail.com>
2018-01-12 21:45:26 +0800

    add server

--- socket_server.py
+++ socket_server.py
@@ -9,62 +9,26 @@ socket service
 """
 
 
-import socket
-import threading
-import time
-import sys
-import os
-import struct
-from io import BytesIO
+import socketserver
 
-def socket_service():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind(('127.0.0.1', 80))
-        s.listen(10)
-    except socket.error as msg:
-        print(msg)
-        sys.exit(1)
-    print('Waiting connection...')
-
-    while 1:
-        conn, addr = s.accept()
-        t = threading.Thread(target=deal_data, args=(conn, addr))
-        t.start()
-
-def deal_data(conn, addr):
-    print('Accept new connection from {0}'.format(addr))
-    #conn.settimeout(500)
-    conn.send(b'Hi, Welcome to the server!')
-
-    while 1:
-        fileinfo_size = struct.calcsize('128sl')
-        buf = conn.recv(fileinfo_size)
-        if buf:
-            filename, filesize = struct.unpack('128sl', buf)
-            fn = filename.strip('\00'.encode())
-            new_filename = os.path.join('./', fn.decode())
-            print('file new name is {0}, filesize if {1}'.format(new_filename,
-                                                                 filesize))
-
-            recvd_size = 0  # 定义已接收文件的大小
-            fp = open(new_filename, 'wb')
-            print('start receiving...')
-
-            while not recvd_size == filesize:
-                if filesize - recvd_size > 1024:
-                    data = conn.recv(1024)
-                    recvd_size += len(data)
-                else:
-                    data = conn.recv(filesize - recvd_size)
-                    recvd_size = filesize
-                fp.write(data)
-            fp.close()
-            print('end receive...')
-        conn.close()
-        break
-
+# Format: name_len      --- one byte
+#         name          --- name_len bytes
+#         data          --- variable length
+# Save data to name into current directory
+class MyTCPHandler (socketserver.StreamRequestHandler):
+        def handle (self):
+                name_len = ord(self.rfile.read(1))
+                name = self.rfile.read(name_len)
+                print("Get request:%s"%name)
+                fd = open(name, 'wb')
+                cont = self.rfile.read(4096)
+                while cont:
+                        fd.write(cont)
+                        cont = self.rfile.read(4096)
+                fd.close()
+                print("Out :%s"%name)
 
 if __name__ == '__main__':
-    socket_service()
+    addr = ('', 80)
+    server = socketserver.TCPServer(addr, MyTCPHandler)
+    server.serve_forever()
